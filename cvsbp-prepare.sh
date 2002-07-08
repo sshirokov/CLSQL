d58e70bf6153ab3c1a59332f622a8b21d112faa6
Kevin M. Rosenberg <kevin@rosenberg.net>
2002-07-08 17:20:04 +0000

    r2193: *** empty log message ***

--- cvsbp-prepare.sh
+++ cvsbp-prepare.sh
@@ -2,19 +2,6 @@
 
 set -e  # abort on error
 
-# Make documentation to put in debian package 
-# This must match prebuilt upstream documentation
-
-if [ -f doc/Makefile ]; then
-  echo "Making upstream documentation for CVS checked-out package"
-  cd doc
-  make all
-  cd ..
-else
-  echo "Unable to build documentation for CVS checked out package"
-  exit 1
-fi
-
 echo "Cleaning checkout CVS directory"
 rm -f upload.sh make-upstream.sh make-debian.sh
 rm -f `find . -type f -name .cvsignore`
