6051747d447703807a48038d9112adfa91e3d4fa
Kevin M. Rosenberg <kevin@rosenberg.net>
2002-07-25 16:13:49 +0000

    r2241: Auto commit for Debian build

--- debian/make-upstream.sh
+++ debian/make-upstream.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
 
-bups.sh -Sclsql -Dcl-sql -f"-name test.config" -d"-name .bin"
+bups.sh -Sclsql -Dcl-sql -f"-name test.config" -d"-name .bin" || 
+  (echo "Error building upstream" >& 2; exit 1)
+
 exit 0
+
