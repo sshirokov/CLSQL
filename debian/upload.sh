a24d3ebbdbb002b32134e369663932174f278e4e
Kevin M. Rosenberg <kevin@rosenberg.net>
2002-08-02 13:46:07 +0000

    r2277: Autocommit for make-debian

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -e
 
-debupload.sh -Sclsql -Dcl-sql -hftp.med-info.com -d/home/ftp/pub/clsql \
-    -B"cl-sql cl-sql-base cl-sql-aodbc cl-sql-mysql cl-sql-postgresql cl-sql-postgresql-socket" \
-    -c"(cd /opt/apache/htdocs/clsql.med-info.com; make)"
+debupload.sh -Sclsql -Dcl-sql -Uftp.med-info.com -R/home/ftp/pub/clsql \
+    -C"(cd /opt/apache/htdocs/clsql.med-info.com; make)"
 
