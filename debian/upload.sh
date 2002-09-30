2cf33a79e15065d4a231658a8912d4f728e11560
Kevin M. Rosenberg <kevin@rosenberg.net>
2002-09-30 06:28:31 +0000

    r2903: *** empty log message ***

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-dup clsql -Uftp.med-info.com -D/home/ftp/clsql \
+dup clsql -Uftp.med-info.com -D/home/ftp/clsql -su \
     -C"(cd /opt/apache/htdocs/clsql.med-info.com; make install-doc)"
 
