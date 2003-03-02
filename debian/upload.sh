ed93192be51252f3f6143ab316baa123dd406928
Kevin M. Rosenberg <kevin@rosenberg.net>
2003-03-02 23:03:49 +0000

    r4150: Auto commit for Debian build

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 dup clsql -Uftp.med-info.com -D/home/ftp/clsql -su \
-    -C"(cd /opt/apache/htdocs/clsql.med-info.com; make install)" $*
+    -C"(cd /opt/apache/htdocs/clsql; make install)" $*
