6154e9896e720bd3a39f8bdd9e034da1d431cdb0
Kevin M. Rosenberg <kevin@rosenberg.net>
2004-04-11 12:38:35 +0000

    r8954: check for any errors and report

--- tests/test-init.lisp
+++ tests/test-init.lisp
@@ -289,8 +289,11 @@
   (clsql:update-records-from-instance employee10)
   (clsql:update-records-from-instance company1))
 
+(defvar *error-count* 0)
+
 (defun run-tests ()
-  (let ((specs (read-specs)))
+  (let ((specs (read-specs))
+	(*error-count* 0))
     (unless specs
       (warn "Not running tests because test configuration file is missing")
       (return-from run-tests :skipped))
@@ -298,7 +301,8 @@
     (dolist (db-type +all-db-types+)
       (let ((spec (db-type-spec db-type specs)))
 	(when spec
-	  (do-tests-for-backend spec db-type))))))
+	  (do-tests-for-backend spec db-type))))
+    (zerop *error-count*)))
 
 (defun load-necessary-systems (specs)
   (dolist (db-type +all-db-types+)
@@ -319,7 +323,8 @@
   (ignore-errors (create-database spec :database-type db-type))
   (with-tests (:name "CLSQL")
     (test-basic spec db-type))
-  
+  (incf *error-count* *test-errors*)
+
   (ignore-errors (destroy-database spec :database-type db-type))
   (ignore-errors (create-database spec :database-type db-type))
   (dolist (test (append *rt-connection* *rt-fddl* *rt-fdml*
@@ -327,5 +332,8 @@
     (eval test))
   (test-connect-to-database db-type spec)
   (test-initialise-database)
-  (rtest:do-tests))
+  (let ((remaining (rtest:do-tests)))
+    (when (consp remaining)
+      (incf *error-count* (length remaining)))))
+
 
