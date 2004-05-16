5dea5b54ed275947c1d78426483acb1a7fb92f21
Kevin M. Rosenberg <kevin@rosenberg.net>
2004-05-16 05:12:10 +0000

    r9367: initial import

---
+++ db-oracle/make.sh
@@ -0,0 +1,10 @@
+if [ -z "$ORACLE_HOME" ]; then 
+  ORACLE_HOME=/opt/10g/product/10.1.0/db_1
+fi
+
+EMPTY_LIBS=-lclntst10
+LIBS="-lagent10 -lagtsh -lclntsh -lclient10 -lvsn10 -lcommon10 -lskgxp10 -lmm -lnls10 -lcore10 -lgeneric10 -ldl -lgcc -lm"
+OBJECTS="rdbms/lib/defopt.o"
+LIBNAME=oracle
+
+gcc -shared -L $ORACLE_HOME -L ${ORACLE_HOME}/lib $ORACLE_HOME/rdbms/lib/homts.o $LIBS -o ${LIBNAME}.so
