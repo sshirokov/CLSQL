32f0258a070695de12e1651dfa1c4ee31337a15e
Kevin M. Rosenberg <kevin@rosenberg.net>
2004-09-28 06:20:21 +0000

    r10042: add umask to upload scripts

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 dup clsql -Uftp.med-info.com -D/home/ftp/clsql -su \
-    -C"(cd /opt/apache/htdocs/clsql; make install)" $*
+    -C"(umask 022; cd /opt/apache/htdocs/clsql; make install)" $*
