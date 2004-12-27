c0f15eada06c0c53437a5b3acc72ab1cd3c72df1
Kevin M. Rosenberg <kevin@rosenberg.net>
2004-12-27 06:06:37 +0000

    r10232: 26 Dec 2004 Kevin Rosenberg <kevin@rosenberg.net>         * doc/ref-fdml.lisp: Fix variable tag name to varname         [noted by Eduardo Munoz]         * db-mysql/mysql-loader.lisp: Handle library paths for 64-bit systems

--- db-mysql/mysql-loader.lisp
+++ db-mysql/mysql-loader.lisp
@@ -20,7 +20,7 @@
 
 (defparameter *clsql-mysql-library-path* 
   (uffi:find-foreign-library
-   '(#+64bit "mysql64" "mysql")
+   '(#+(or 64bit x86-64) "mysql64" "mysql")
    `(,clsql-mysql-system::*library-file-dir*
      "/usr/lib/clsql/"
      "/sw/lib/clsql/")
@@ -32,7 +32,10 @@
 (defparameter *mysql-library-candidate-directories*
     `(,(pathname-directory *load-pathname*)
       "/opt/mysql/lib/mysql/" "/usr/local/lib/"
-      #+64bit "/usr/lib64/" #+64bit "/usr/local/lib64/mysql/"
+      #+(or 64bit x86-64) "/usr/lib64/" #+(or 64bit x86-64) "/usr/local/lib64/mysql/"
+      #+(or 64bit x86-64) "/usr/local/lib64/"
+      #+(or 64bit x86-64) "/usr/local/lib64/mysql/"
+
       "/usr/lib/" "/usr/local/lib/mysql/" "/usr/lib/mysql/" "/mysql/lib/opt/" "/sw/lib/mysql/" "/opt/local/lib/mysql/"))
 
 (defvar *mysql-library-candidate-drive-letters* '("C" "D" "E"))
