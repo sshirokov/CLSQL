ecd05e5b70c793cce2ce44f3bb4d4a7ecbc05153
Kevin M. Rosenberg <kevin@rosenberg.net>
2005-06-09 00:34:43 +0000

    r10593: Automated commit for Debian build of clsql upstream-version-3.2.0

--- uffi/make.sh
+++ uffi/make.sh
@@ -12,7 +12,7 @@ esac
     
 if [ "$os_linux" -o "$os_freebsd" ]; then
     gcc -fPIC -DPIC -c $SOURCE -o $OBJECT
-    ld -shared -soname=$BASE $OBJECT -o $SHARED_LIB
+    ld -shared -soname=$BASE $LDFLAGS $OBJECT -o $SHARED_LIB
 elif [ "$os_darwin" ]; then
     cc -dynamic -c $SOURCE -o $OBJECT
     ld -bundle /usr/lib/bundle1.o -flat_namespace -undefined suppress -o $BASE.dylib $OBJECT
