8032ca50e601511d03908b6d20c7fd3fb9725437
Kevin M. Rosenberg <kevin@rosenberg.net>
2005-09-08 15:22:14 +0000

    r10700: fix xargs chmod

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 dup clsql -Uftp.med-info.com -D/home/ftp/clsql -su \
-    -C"(umask 022; cd /opt/apache/htdocs/clsql; make install; find . -type d |xargs go+rx; find . -type f | xargs go+r)" $*
+    -C"(umask 022; cd /opt/apache/htdocs/clsql; make install; find . -type d |xargs chmod go+rx; find . -type f | xargs chmod go+r)" $*
