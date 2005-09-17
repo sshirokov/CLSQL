a995b49f3462e35a432bae3340fe6411498e16a6
Kevin M. Rosenberg <kevin@rosenberg.net>
2005-09-17 22:18:06 +0000

    r10735:

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-dup clsql -Uftp.med-info.com -D/home/ftp/clsql -su \
+dup clsql -Ufiles.b9.com -D/home/ftp/clsql -su \
     -C"(umask 022; cd /opt/apache/htdocs/clsql; make install; find . -type d |xargs chmod go+rx; find . -type f | xargs chmod go+r)" $*
