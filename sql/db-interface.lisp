e9e1a810cd690a31c0258f1aad313aeae18a8906
Kevin Rosenberg <kevin@rosenberg.net>
2009-09-04 11:57:30 -0600

    Fix transposed letters

--- sql/db-interface.lisp
+++ sql/db-interface.lisp
@@ -141,17 +141,14 @@ if not found. May signal an error if unable to communicate with database server.
   (:documentation
    "Lists all databases found for TYPE. May signal an error if unable to communicate with database server."))
 
-(defgeneric database-destroy (connection-spec database)
-  (:documentation "Destroys (drops) a database."))
-
 (defgeneric database-truncate (database)
   (:method ((database t))
     (signal-no-database-error database))
   (:documentation "Remove all data from database."))
 
-(defgeneric database-destory (connection-spec type)
+(defgeneric database-destroy (connection-spec type)
   (:documentation
-   "Destroys a database, returns T if successfull or signals an error
+   "Destroys (drops) a database, returns T if successfull or signals an error
 if unable to destory."))
 
 (defgeneric database-create-sequence (name database)
