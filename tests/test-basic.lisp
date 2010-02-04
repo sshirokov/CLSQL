4a2e3a71a1ce79482ba704ec815013de2c986274
Nathan Bird <nathan@acceleration.net>
2010-02-04 09:44:00 -0500

    Move i18n tests to separate file until we have a better system of dealing with them. Also added some comments to properly documenting them as multi-byte tests and describe their properties a bit better.

--- tests/test-basic.lisp
+++ tests/test-basic.lisp
@@ -227,16 +227,6 @@
 			"mismatch on a. inserted: ~a returned: ~a" len (length a)))
 	      )))
       nil)
-
-    (deftest :basic/i18n/1
-	(first (query "SELECT 'Iñtërnâtiônàlizætiøn'"
-		      :flatp t :field-names nil))
-      "Iñtërnâtiônàlizætiøn")
-
-    (deftest :basic/i18n/2
-	(first (query "SELECT 'Iñtërnâtiônàližætiøn'"
-		      :flatp t :field-names nil))
-      "Iñtërnâtiônàližætiøn")
     ))
 
 
