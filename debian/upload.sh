069c3dc63ca6996667214c187f770c1aee2b65c4
Kevin M. Rosenberg <kevin@rosenberg.net>
2011-03-29 18:36:22 -0600

    Update directory

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 dup clsql -Ufiles.b9.com -D/home/ftp/clsql -su \
-    -C"(umask 022; cd /opt/apache/htdocs/clsql; make install; find . -type d |xargs chmod go+rx; find . -type f | xargs chmod go+r)" $*
+    -C"(umask 022; cd /srv/www/html/clsql; make install; find . -type d |xargs chmod go+rx; find . -type f | xargs chmod go+r)" $*
