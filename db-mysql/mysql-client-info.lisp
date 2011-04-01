31ea2096212455c929812d2f6dada1db5b6808db
Kevin M. Rosenberg <kevin@rosenberg.net>
2011-04-01 10:44:53 -0600

    Recognize v6 of MySQL client library

--- db-mysql/mysql-client-info.lisp
+++ db-mysql/mysql-client-info.lisp
@@ -44,6 +44,8 @@
        (when (and (>= (length *mysql-client-info*) 3)
                   (string-equal "5.1" *mysql-client-info* :end2 3))
          (pushnew :mysql-client-v5.1 cl:*features*)))
+      ((eql (schar *mysql-client-info* 0) #\6)
+       (pushnew :mysql-client-v6 cl:*features*))
       (t
        (error "Unknown mysql client version '~A'." *mysql-client-info*)))))
 
